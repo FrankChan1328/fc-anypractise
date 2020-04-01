4882a7b956e99b88adab06bcad9b7b2409f9e6c3
Frank <frankchen1328@gmail.com>
2020-04-01 23:16:58 +0800

    添加注釋

--- src/main/java/thread/threadlocal/ThreadLocalDemo.java
+++ src/main/java/thread/threadlocal/ThreadLocalDemo.java
@@ -2,6 +2,12 @@ package thread.threadlocal;
 
 import java.util.Random;
 
+/**
+ * ThreadLocal使用的一般步骤:
+ * <p><1>.在多线程的类(如ThreadDemo类)中，创建一个ThreadLocal对象threadXxx，用来保存线程间需要隔离处理的对象xxx。
+ * <p><2>.在ThreadDemo类中，创建一个获取要隔离访问的数据的方法getXxx()，在方法中判断，若ThreadLocal对象为null时候，应该new()一个隔离访问类型的对象，并强制转换为要应用的类型。
+ * <p><3>.在ThreadDemo类的run()方法中，通过getXxx()方法获取要操作的数据，这样可以保证每个线程对应一个数据对象，在任何时刻都操作的是这个对象。
+ */
 public class ThreadLocalDemo implements Runnable {
 	// 创建线程局部变量studentLocal，在后面你会发现用来保存Student对象
 	private final static ThreadLocal<Student> studentLocal = new ThreadLocal<>();
