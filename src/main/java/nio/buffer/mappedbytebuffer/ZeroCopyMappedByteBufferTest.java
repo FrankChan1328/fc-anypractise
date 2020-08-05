6e06bef14aee9235e775ccb84cc0da7eb6293721
Frank <frankchen1328@gmail.com>
2020-08-06 00:23:25 +0800

    add

---
+++ src/main/java/nio/buffer/mappedbytebuffer/ZeroCopyMappedByteBufferTest.java
@@ -0,0 +1,27 @@
+package nio.buffer.mappedbytebuffer;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Scanner;
+
+public class ZeroCopyMappedByteBufferTest {
+
+	public static void main(String[] args) throws Exception {
+		File file = new File("1.txt");
+		long len = file.length();
+		byte[] ds = new byte[(int) len];
+		MappedByteBuffer mappedByteBuffer = new FileInputStream(file).getChannel().map(FileChannel.MapMode.READ_ONLY, 0,
+				len);
+		for (int offset = 0; offset < len; offset++) {
+			byte b = mappedByteBuffer.get();
+			ds[offset] = b;
+		}
+		Scanner scan = new Scanner(new ByteArrayInputStream(ds)).useDelimiter(" ");
+		while (scan.hasNext()) {
+			System.out.print(scan.next() + " ");
+		}
+	}
+}
